f548eb7fcb3a43d6bc7ff1705c265b0847906e9b
fifthThirteen <30660189+fifthThirteen@users.noreply.github.com>
2018-02-09 17:14:09 +0800

    1
    1

--- js/tab_switch.js
+++ js/tab_switch.js
@@ -4,9 +4,11 @@
   tab_t_tag:选项卡元素标签
   tab_c:切换内容容器ID
   tab_c_tag:切换内容元素标签
+  hide:div隐藏css类
+  act:选项卡激活css类
   evt:事件方法--onclick等事件不支持传参
 */
-function tab(tab_t,tab_t_tag,tab_c,tag_c_tag,evt){
+function tab(tab_t,tab_t_tag,act,tab_c,tag_c_tag,hide,evt){
     var tab_t=document.getElementById(tab_t);
     var tab_t_li=tab_t.getElementsByTagName(tab_t_tag);
     var tab_c=document.getElementById(tab_c);
@@ -18,9 +20,9 @@ function tab(tab_t,tab_t_tag,tab_c,tag_c_tag,evt){
         var fun = function(){
             for(i=0;i<len;i++){
                 tab_t_li[i].className='';
-                tab_c_li[i].className='hide';
+                tab_c_li[i].setAttribute('class',hide);
             }
-            tab_t_li[this.index].className='act';
+            tab_t_li[this.index].setAttribute('class',act);
             tab_c_li[this.index].className='';
         }
         addEvent(tab_t_li[i],evt,fun);
